12257cd384afc34c1fe1a305a3216fed95aba913
steves3142 <steves3142@gmail.com>
2022-10-16 22:21:02 -0400

    motion.div added to icons

--- portfolio-2.0/components/Header.tsx
+++ portfolio-2.0/components/Header.tsx
@@ -1,39 +1,69 @@
 import React from 'react'
-import { SocialIcon } from "react-social-icons"; 
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion"
 
 type Props = {}
 
-export default function Header({}: Props) {
-  return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 
-    xl:items-center">
-        <div className="flex flex-row items-center">
-            {/* Social Icons */}
-            <SocialIcon url="https://twitter.com/SteveSusanibar"
-            fgColor="gray"
-            bgColor="transparent"
-            />
-            <SocialIcon
-            url="https://www.linkedin.com/in/steve-susanibar/"
-            fgColor="gray"
-            bgColor="transparent"
-            />
-            <SocialIcon
-            url="https://github.com/steves3142"
-            fgColor="gray"
-            bgColor="transparent"
-            />
-        </div>
+export default function Header({ }: Props) {
+    return (
+        <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
 
-        <div className="flex flex-row items-center text-gray-300 cursor-pointer">
-        <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="gray"
-            bgColor="transparent"
-            />
-            <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-        </div>
-    </header>
-  )
+                }}
+                className="flex flex-row items-center"
+            >
+
+                {/* Social Icons */}
+                <SocialIcon url="https://twitter.com/SteveSusanibar"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://www.linkedin.com/in/steve-susanibar/"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <SocialIcon
+                    url="https://github.com/steves3142"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+            </motion.div>
+
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{ duration: 1.5 }}
+                className="flex flex-row items-center text-gray-300 cursor-pointer"
+            >
+                <SocialIcon
+                    className="cursor-pointer"
+                    network="email"
+                    fgColor="gray"
+                    bgColor="transparent"
+                />
+                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
+            </motion.div>
+        </header>
+    )
 }
