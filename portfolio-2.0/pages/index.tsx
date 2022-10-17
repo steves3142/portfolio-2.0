10dfe974bb402ec3faf9575aab162fcabf2b1153
steves3142 <steves3142@gmail.com>
2022-10-16 23:16:58 -0400

    background circles

--- portfolio-2.0/pages/index.tsx
+++ portfolio-2.0/pages/index.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
       <Head>
         <title>Steve's Portfolio</title>
       </Head>  
 
-      {/* Header */}
       <Header />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       { /* About */ }
 
