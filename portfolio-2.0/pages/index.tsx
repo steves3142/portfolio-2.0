04cf5dcd730c7bbc706f5ff6af8cf4d0b255d2cf
steves3142 <steves3142@gmail.com>
2022-10-18 13:33:26 -0400

    installed tailwind scrollbar

--- portfolio-2.0/pages/index.tsx
+++ portfolio-2.0/pages/index.tsx
@@ -10,7 +10,7 @@ import ContactMe from '../components/ContactMe';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Head>
         <title>Steve's Portfolio</title>
       </Head>  
