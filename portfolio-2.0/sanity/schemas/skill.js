776b289cf15353ea11cf94526f3a857349cabd4c
steves3142 <steves3142@gmail.com>
2022-10-20 00:36:40 -0400

    changing sanity schema file names

---
+++ portfolio-2.0/sanity/schemas/skill.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'pageInfo',
+  title: 'PageInfo',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+  ],
+}
