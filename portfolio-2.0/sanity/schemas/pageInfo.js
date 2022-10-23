d27e9ce65abbb7d3198ef30aad5475fc087d47b1
steves3142 <steves3142@gmail.com>
2022-10-22 23:34:17 -0400

    compiled sanity binary files and updated pageInfo schema

--- portfolio-2.0/sanity/schemas/pageInfo.js
+++ portfolio-2.0/sanity/schemas/pageInfo.js
@@ -9,6 +9,11 @@ export default {
       type: "string",
     },
     {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+    {
       name: "heroImage",
       title: "Image",
       type: "image",
